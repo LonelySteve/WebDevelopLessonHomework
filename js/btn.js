6d9edad062f2d5d8da2aef4cde5e898b73c9b01d
JLoeve <jianglufull@foxmail.com>
2019-04-27 17:06:44 +0800

    作业要求基本完成

---
+++ js/btn.js
@@ -0,0 +1,31 @@
+  function get_selected_items_num() {
+      return $(".checkbox input[type='checkbox']:checked").length;
+  }
+
+  $(document).ready(
+      function () {
+          var pd = function (event) {
+              event.preventDefault();
+          };
+
+          if (get_selected_items_num() == 0) {
+              $(".disabled").click(pd);
+          } else {
+              $(".disabled").off("click");
+              $("#btn-submit").removeClass("disabled");
+          }
+
+          $(".checkbox input[type='checkbox']").change(
+              function () {
+                  $this = $(this);
+                  if (get_selected_items_num() == 0) {
+                      $(".disabled").click(pd);
+                      $("#btn-submit").addClass("disabled");
+                  } else {
+                      $(".disabled").off("click");
+                      $("#btn-submit").removeClass("disabled");
+                  }
+              }
+          )
+      }
+  );
